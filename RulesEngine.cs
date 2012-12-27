8e8b3331f7c12acc25169a57f24afe23d36ed8d8
Aleš Roubíček <ales@roubicek.name>
2012-12-27 13:52:19 +0100

    Refactoring

--- RulesEngine.cs
+++ RulesEngine.cs
@@ -1,15 +1,20 @@
 namespace CorrespondenceCoderetreat
 {
+    using System.Linq;
+
     public class RulesEngine
     {
+        static readonly int[] SurviveRules = { 2, 3 };
+        static readonly int[] BornRules = { 3 };
+
         public bool WillSurvive(int neighborsCount)
         {
-            return neighborsCount == 2 || neighborsCount == 3;
+            return SurviveRules.Contains(neighborsCount);
         }
 
         public bool WillBorn(int neighborsCount)
         {
-            return neighborsCount == 3;
+            return BornRules.Contains(neighborsCount);
         }
     }
 }
