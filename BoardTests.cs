fa17648377ea9a270883d0e9412a40b36041ba16
Michal Tehnik <michal.tehnik@mictech.cz>
2013-01-06 13:40:29 +0100

    Your turn

--- BoardTests.cs
+++ BoardTests.cs
@@ -1,15 +1,31 @@
-﻿using NUnit.Framework;
+﻿using System.Collections.Generic;
+using NUnit.Framework;
 
 namespace CorrespondenceCoderetreat
 {
     [TestFixture]
     public class BoardTests
     {
+        readonly HashSet<Coordinates> coordinatesOfLivingCells;
+
+        public BoardTests()
+        {
+            coordinatesOfLivingCells = new HashSet<Coordinates> { new Coordinates(5, 2) };
+        }
+
         [Test]
         public void ShouldCountZeroNeighborsOnEmptyBoard()
         {
             var board = new Board(Generation.Empty);
             Assert.AreEqual(0, board.CountNeightborsFor(new Coordinates(5, 3)));
+        }
+
+        [Test]
+        public void ShouldCountOneNeighborForCoordinatesX5Y3()
+        {
+            var generation = new Generation(coordinatesOfLivingCells);
+            var board = new Board(generation);
+            Assert.AreEqual(1, board.CountNeightborsFor(new Coordinates(5, 3)));
         }
     }
 }
--- Generation.cs
+++ Generation.cs
@@ -1,7 +1,20 @@
+using System.Collections.Generic;
+
 namespace CorrespondenceCoderetreat
 {
     public class Generation
     {
         public static readonly Generation Empty = new Generation();
+        readonly HashSet<Coordinates> coordinatesOfLivingCells;
+
+        public Generation()
+        {
+            
+        }
+
+        public Generation(HashSet<Coordinates> coordinatesOfLivingCells)
+        {
+            this.coordinatesOfLivingCells = coordinatesOfLivingCells;
+        }
     }
 }
